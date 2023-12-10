53ab9a3ba90c6eb32ed5ca494835293c6f15285c
PRStartGit <hello@prstart.co.uk>
2023-12-10 23:49:18 +0000

    Samu itt van

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -10,10 +10,29 @@ const Dashboard = () => {
       {user ? ( // Check if user is authenticated
         <>
           <h1>Dashboard!</h1>
-          <div>
-          <img class="rounded-circle" src={user.photoURL} alt={user.displayName} />
-          <span> {user.displayName}</span>{/* Add your dashboard content here */}
-          </div>
+          <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-3">
+          <div className="card">
+              <img
+                src={user.photoURL || '/default-avatar.png'} // Use a default avatar image if photoURL is not available
+                className="card-img-top"
+                alt={user.displayName}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{user.displayName}</h5>
+                <p className="card-text">Email: {user.email}</p>
+                {/* Add more user information here */}
+                <p className="card-text">Date of Birth: {user.dateOfBirth}</p>
+                <p className="card-text">Address: {user.address}</p>
+              </div>
+              <div className="card-footer">
+                
+              </div>
+             </div> 
+             </div> 
+             </div> 
+             </div> 
         </>
       ) : (
         <h1>You need to be logged in to see this page.</h1>
